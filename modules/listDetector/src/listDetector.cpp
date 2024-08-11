2f0ed45d253b9fdc81e71f1e8a3c3e8725bdee9d
Damir Zainullin <zaidamilda@gmail.com>
2024-08-11 15:01:52 +0500

    ListDetector - Introduce ListDetector class

---
+++ modules/listDetector/src/listDetector.cpp
@@ -0,0 +1,85 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Daniel Pelanek <xpeland00@vutbr.cz>
+ * @brief Implementation of the ListDetector class.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "listDetector.hpp"
+
+#include <algorithm>
+#include <optional>
+#include <regex>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+namespace ListDetector {
+
+static telemetry::Content createRuleTelemetryContent(const Rule& rule)
+{
+	const RuleStats& ruleStats = rule.getStats();
+	telemetry::Dict dict;
+	dict["matchedCount"] = telemetry::Scalar(ruleStats.matchedCount);
+	return dict;
+}
+
+ListDetectorMode ListDetector::convertStringToListDetectorMode(const std::string& str)
+{
+	if (str != "bl" && str != "wl" && str != "blacklist" && str != "whitelist") {
+		throw std::runtime_error(
+			"Unknown list mode. Only allowed values are blacklist and whitelist");
+	}
+
+	if (str == "bl" || str == "blacklist") {
+		return ListDetectorMode::BLACKLIST;
+	}
+	return ListDetectorMode::WHITELIST;
+}
+
+ListDetector::ListDetector(const ConfigParser* configParser, ListDetectorMode mode)
+	: m_mode(mode)
+	, m_rulesMatcher(configParser)
+{
+}
+
+bool ListDetector::matches(const Nemea::UnirecRecordView& unirecRecordView)
+{
+	const bool match = m_rulesMatcher.anyOfRuleMatches(unirecRecordView);
+
+	if (m_mode == ListDetectorMode::WHITELIST) {
+		return match;
+	}
+	return !match;
+}
+
+void ListDetector::setTelemetryDirectory(const std::shared_ptr<telemetry::Directory>& directory)
+{
+	m_holder.add(directory);
+
+	auto rulesDirectory = directory->addDir("rules");
+	const std::vector<Rule>& rules = m_rulesMatcher.getRules();
+
+	for (size_t ruleIndex = 0; ruleIndex < rules.size(); ruleIndex++) {
+		const Rule& rule = rules.at(ruleIndex);
+		const telemetry::FileOps fileOps
+			= {[&rule]() { return createRuleTelemetryContent(rule); }, nullptr};
+		auto ruleFile = rulesDirectory->addFile(std::to_string(ruleIndex), fileOps);
+		m_holder.add(ruleFile);
+	}
+
+	const telemetry::AggOperation aggFileOps = {
+		telemetry::AggMethodType::SUM,
+		"matchedCount",
+		"totalMatchedCount",
+	};
+
+	auto aggFile = directory->addAggFile("aggStats", "rules/.*", {aggFileOps});
+	m_holder.add(aggFile);
+}
+
+} // namespace ListDetector
---
+++ modules/listDetector/src/listDetector.hpp
@@ -0,0 +1,70 @@
+/**
+ * @file
+ * @author Pavel Siska <siska@cesnet.cz>
+ * @author Daniel Pelanek <xpeland00@vutbr.cz>
+ * @brief Declaration of the ListDetector class.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#include "configParser.hpp"
+#include "rulesMatcher.hpp"
+
+#include <memory>
+#include <telemetry.hpp>
+#include <unirec++/unirec.hpp>
+#include <vector>
+
+namespace ListDetector {
+
+/**
+ * @brief Possible modes of the ListDetector.
+ */
+enum class ListDetectorMode : uint8_t {
+	BLACKLIST, ///< Only records that match some rule in rule list are forwarded.
+	WHITELIST ///< Only records that do not match any rule in rule list are forwarded.
+};
+
+/**
+ * @brief Represents a ListDetector for Nemea++ records.
+ */
+class ListDetector {
+public:
+	/**
+	 * @brief Constructor for ListDetector.
+	 * @param configParser Pointer to the ConfigParser providing rules.
+	 * @param mode Mode to use.
+	 */
+	explicit ListDetector(const ConfigParser* configParser, ListDetectorMode mode);
+
+	/**
+	 * @brief Checks if the given UnirecRecordView matches some rule from ListDetector.
+	 * @param unirecRecordView The Unirec record to check against the ListDetector.
+	 * @return True if matches, false otherwise.
+	 */
+	bool matches(const Nemea::UnirecRecordView& unirecRecordView);
+
+	/**
+	 * @brief Sets the telemetry directory for the ListDetector.
+	 * @param directory directory for ListDetector telemetry.
+	 */
+	void setTelemetryDirectory(const std::shared_ptr<telemetry::Directory>& directory);
+
+	/**
+	 * @brief Converts provided string to the list detector mode.
+	 * @param str String to convert.
+	 * @return List derector mode specified by string.
+	 */
+	static ListDetectorMode convertStringToListDetectorMode(const std::string& str);
+
+private:
+	telemetry::Holder m_holder;
+
+	ListDetectorMode m_mode;
+
+	RulesMatcher m_rulesMatcher;
+};
+
+} // namespace ListDetector

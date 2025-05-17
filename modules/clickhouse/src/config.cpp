276fd800dad73185f9768ac73432ec711f7f402f
Daniel Pelanek <xpeland00@hothandle.liberouter.org>
2025-05-17 17:17:02 +0200

    clickhouse - introduce config and parsing for it Adds config for clickhouse database connection, number of insterter threads, interface data template accepted as input etc. Added rapidxml library for parsing config from xml.

---
+++ modules/clickhouse/src/config.cpp
@@ -0,0 +1,263 @@
+/**
+ * @file config.cpp
+ * @author Daniel Pelanek <xpeland00@vutbr.cz>
+ * @brief Parses config xml into config structure. Uses rapidxml.
+ *
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "config.hpp"
+#include "rapidxml.hpp"
+
+#include <algorithm>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <stdexcept>
+#include <utility>
+
+/**
+ * @brief Remove leading spaces from string.
+ *
+ * @param str
+ */
+static inline void trimLeft(std::string& str)
+{
+	str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](unsigned char chr) {
+				  return std::isspace(chr) == 0;
+			  }));
+}
+
+/**
+ * @brief Remove spaces from string on both sides.
+ *
+ * @param str
+ */
+static inline void trim(std::string& str)
+{
+	str.erase(
+		std::find_if(
+			str.rbegin(),
+			str.rend(),
+			[](unsigned char chr) { return std::isspace(chr) == 0; })
+			.base(),
+		str.end());
+
+	str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](unsigned char chr) {
+				  return std::isspace(chr) == 0;
+			  }));
+}
+
+static int parseInteger(const char* intString)
+{
+	return std::stoi(std::string(intString));
+}
+
+static Config::Endpoint parseEndpoint(rapidxml::xml_node<>* endpointNode)
+{
+	Config::Endpoint endpoint;
+
+	for (rapidxml::xml_node<>* node = endpointNode->first_node(); node != nullptr;
+		 node = node->next_sibling()) {
+		if (strcmp(node->name(), "host") == 0) {
+			endpoint.host = node->value();
+
+		} else if (strcmp(node->name(), "port") == 0) {
+			endpoint.port = parseInteger(node->value());
+
+		} else {
+			std::stringstream sstream;
+			sstream << "Invalid endpoint parameter: " << node->name();
+			throw std::runtime_error(sstream.str());
+		}
+	}
+
+	return endpoint;
+}
+
+static void parseEndpoints(rapidxml::xml_node<>* endpointsNode, Config& config)
+{
+	for (rapidxml::xml_node<>* node = endpointsNode->first_node(); node != nullptr;
+		 node = node->next_sibling()) {
+		if (strcmp(node->name(), "endpoint") == 0) {
+			config.connection.endpoints.push_back(parseEndpoint(node));
+
+		} else {
+			std::stringstream sstream;
+			sstream << "Endpoints can only have endpoint not: " << node->name();
+			throw std::runtime_error(sstream.str());
+		}
+	}
+}
+
+/**
+ * @brief type from unirec template into local enum.
+ *
+ */
+static const std::map<std::string, ColumnType> g_string_to_columntype
+	= {{"int8", ColumnType::INT8},       {"int8*", ColumnType::INT8_ARR},
+	   {"int16", ColumnType::INT16},     {"int16*", ColumnType::INT16_ARR},
+	   {"int32", ColumnType::INT32},     {"int32*", ColumnType::INT32_ARR},
+	   {"int64", ColumnType::INT64},     {"int64*", ColumnType::INT64_ARR},
+	   {"uint8", ColumnType::UINT8},     {"uint8*", ColumnType::UINT8_ARR},
+	   {"uint16", ColumnType::UINT16},   {"uint16*", ColumnType::UINT16_ARR},
+	   {"uint32", ColumnType::UINT32},   {"uint32*", ColumnType::UINT32_ARR},
+	   {"uint64", ColumnType::UINT64},   {"uint64*", ColumnType::UINT64_ARR},
+	   {"char", ColumnType::CHAR},       {"char*", ColumnType::CHAR_ARR},
+	   {"float", ColumnType::FLOAT},     {"float*", ColumnType::FLOAT_ARR},
+	   {"double", ColumnType::DOUBLE},   {"double*", ColumnType::DOUBLE_ARR},
+	   {"ipaddr", ColumnType::IPADDR},   {"ipaddr*", ColumnType::IPADDR_ARR},
+	   {"macaddr", ColumnType::MACADDR}, {"macaddr*", ColumnType::MACADDR_ARR},
+	   {"time", ColumnType::TIME},       {"time*", ColumnType::TIME_ARR},
+	   {"string", ColumnType::STRING},   {"bytes", ColumnType::BYTES}};
+
+static void parseColumns(rapidxml::xml_node<>* columnsNode, Config& config)
+{
+	std::stringstream csvString(columnsNode->value());
+	std::string curCsvValue;
+
+	while (std::getline(csvString, curCsvValue, ',')) {
+		// Type/Name can't have space. Trim leading and trailing spaces.
+		// Leading spaces for name are trimmed below.
+		trim(curCsvValue);
+
+		Config::Column column;
+		size_t const spacePos = curCsvValue.find(' ');
+
+		std::string const type = curCsvValue.substr(0, spacePos);
+		std::string name = curCsvValue.substr(spacePos + 1);
+
+		try {
+			column.type = g_string_to_columntype.at(type);
+
+		} catch (std::out_of_range& ex) {
+			std::stringstream sstream;
+			sstream << "Incorrect column type: " << curCsvValue.substr(0, spacePos);
+			throw std::runtime_error(sstream.str());
+		}
+
+		trimLeft(name);
+		column.name = name;
+
+		column.fieldID = 0;
+
+		config.columns.push_back(column);
+
+		// Template stored in input interface format. For ensuring format.
+		config.templateColumnCsv += type;
+		config.templateColumnCsv += " ";
+		config.templateColumnCsv += name;
+		config.templateColumnCsv += ",";
+	}
+
+	// Trailing comma
+	config.templateColumnCsv.pop_back();
+}
+
+static void parseConnection(rapidxml::xml_node<>* connectionNode, Config& config)
+{
+	for (rapidxml::xml_node<>* node = connectionNode->first_node(); node != nullptr;
+		 node = node->next_sibling()) {
+		if (strcmp(node->name(), "endpoints") == 0) {
+			parseEndpoints(node, config);
+
+		} else if (strcmp(node->name(), "user") == 0) {
+			config.connection.user = node->value();
+
+		} else if (strcmp(node->name(), "password") == 0) {
+			config.connection.password = node->value();
+
+		} else if (strcmp(node->name(), "database") == 0) {
+			config.connection.database = node->value();
+
+		} else if (strcmp(node->name(), "table") == 0) {
+			config.connection.table = node->value();
+
+		} else {
+			std::stringstream sstream;
+			sstream << "Incorrect connection argument name: " << node->name();
+			throw std::runtime_error(sstream.str());
+		}
+	}
+}
+
+static void parseParams(rapidxml::xml_node<>* paramsNode, Config& config)
+{
+	for (rapidxml::xml_node<>* node = paramsNode->first_node(); node != nullptr;
+		 node = node->next_sibling()) {
+		if (strcmp(node->name(), "connection") == 0) {
+			parseConnection(node, config);
+
+		} else if (strcmp(node->name(), "blocks") == 0) {
+			config.blocks = parseInteger(node->value());
+
+		} else if (strcmp(node->name(), "inserterThreads") == 0) {
+			config.inserterThreads = parseInteger(node->value());
+
+		} else if (strcmp(node->name(), "blockInsertThreshold") == 0) {
+			config.blockInsertThreshold = parseInteger(node->value());
+
+		} else if (strcmp(node->name(), "blockInsertMaxDelaySecs") == 0) {
+			config.blockInsertMaxDelaySecs = parseInteger(node->value());
+
+		} else if (strcmp(node->name(), "columns") == 0) {
+			parseColumns(node, config);
+
+		} else {
+			std::stringstream sstream;
+			sstream << "Incorrect argument argument name: " << node->name();
+			throw std::runtime_error(sstream.str());
+		}
+	}
+}
+
+static void parseRoot(rapidxml::xml_node<>* rootNode, Config& config)
+{
+	// Root element has to be output
+	if (strcmp(rootNode->name(), "output") != 0) {
+		throw std::runtime_error("Malformatted xml");
+	}
+
+	for (rapidxml::xml_node<>* node = rootNode->first_node(); node != nullptr;
+		 node = node->next_sibling()) {
+		if (strcmp(node->name(), "params") == 0) {
+			parseParams(node, config);
+		}
+	}
+}
+
+std::string loadFile(const std::string& filename)
+{
+	std::ifstream const file(filename);
+
+	if (!file) {
+		std::stringstream sstream;
+		sstream << "Could not open config file: " << filename;
+		throw std::runtime_error(sstream.str());
+	}
+
+	std::ostringstream buffer;
+	buffer << file.rdbuf();
+	return buffer.str();
+}
+
+Config parseConfig(const std::string& filename)
+{
+	std::string xmlString = loadFile(filename);
+
+	Config config {};
+
+	rapidxml::xml_document<> doc;
+
+	doc.parse<0>(xmlString.data());
+
+	parseRoot(doc.first_node(), config);
+
+	return config;
+}
--- modules/clickhouse/src/main.cpp
+++ modules/clickhouse/src/main.cpp
@@ -1,8 +1,25 @@
+/**
+ * @file main.cpp
+ * @author Daniel Pelanek <xpeland00@vutbr.cz>
+ * @brief Clickhouse Module: resend flowdata to clickhouse
+ *
+ * This file contains the main function and supporting functions for the Unirec Clickhouse Module.
+ * This module takes Unirec records from a unidirectional interface, converts them to
+ * Clickhouse format buffers them and then sends them to the specified Clickhouse server in config.
+ * It utilizes the Unirec++ library for record handling, argparse for command-line argument parsing,
+ * rapidxml for config parsing and clickhouse cpp library.
+ * Ported from ipfixcol2 clickhouse plugin.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include "logger/logger.hpp"
+#include "config.hpp"
 
 #include <unirec++/unirec.hpp>
 #include <argparse/argparse.hpp>
 #include <clickhouse/client.h>
+#include <csignal>
 #include <stdexcept>
 #include <iostream>
 
@@ -48,10 +65,17 @@ void processUnirecRecords(UnirecInputInterface& interface)
 int main(int argc, char** argv) {
     argparse::ArgumentParser program("Unirec Clickhouse");
 
+    program.add_argument("-c", "--config")
+		.required()
+		.help("specify the xml config file. Format is in readme.")
+		.metavar("xml_file");
+
     Unirec unirec({1, 0, "clickhouse", "Unirec clickhouse module"});
 
     Nm::loggerInit();
 	auto logger = Nm::loggerGet("main");
+
+    signal(SIGINT, signalHandler);
 
     try {
 		unirec.init(argc, argv);
@@ -64,11 +88,24 @@ int main(int argc, char** argv) {
 	}
 
     try {
+		program.parse_args(argc, argv);
+	} catch (const std::exception& ex) {
+		logger->error(ex.what());
+		return EXIT_FAILURE;
+	}
+
+    Config config;
+	try {
+		config = parseConfig(program.get<std::string>("--config"));
+	} catch (const std::exception& ex) {
+		logger->error(ex.what());
+		return EXIT_FAILURE;
+	}
+
+    try {
 		UnirecInputInterface interface = unirec.buildInputInterface();
 
 		processUnirecRecords(interface);
-
-		logger->info("here");
 
 	} catch (std::exception& ex) {
 		logger->error(ex.what());

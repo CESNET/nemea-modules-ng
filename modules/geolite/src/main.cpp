b3cc2f44d3acb5df1ea016ec6b7ab7c646c0dec9
Tomas Vrana <xvranat00@stud.fit.vut.cz>
2025-07-21 19:09:13 +0000

    adding main

--- modules/geolite/src/main.cpp
+++ modules/geolite/src/main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
-int main()
+#include <argparse/argparse.hpp>
+#include <unirec++/unirec.hpp>
+
+#define DEFAULT_DIRECTION true
+
+int main(int argc, char** argv)
 {
-	std::cout << "Hello, world!\n";
+	argparse::ArgumentParser program("Geolite");
+
+	Nemea::Unirec unirec({1, 1, "geolite", "Geolite module"});
+
+	try {
+		unirec.init(argc, argv);
+	} catch (const Nemea::HelpException& ex) {
+		std::cerr << program;
+		return EXIT_SUCCESS;
+	}
+
+	// TODO: make it load both ip directions
+
+	try {
+		program.add_argument("-d", "--direction")
+			.help("If specified both IP directions used for geolocation")
+			.default_value(DEFAULT_DIRECTION)
+			.implicit_value(!DEFAULT_DIRECTION);
+		program.add_argument("-f", "--field")
+			.required()
+			.help("Name of Unirec field with IP address")
+			.default_value(std::string("SRC_IP"));
+		program.parse_args(argc, argv);
+	} catch (const std::exception& ex) {
+		std::cerr << program;
+		return EXIT_FAILURE;
+	}
+
+	auto direction = program.get<bool>("--direction");
+	auto field = program.get<std::string>("--field");
+
+	std::cout << "--field: " << field << '\n';
+	std::cout << "--direction: " << direction << '\n';
+
 	return 0;
 }

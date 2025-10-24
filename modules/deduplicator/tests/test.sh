f3d3c34ace81b1cf7e2aa51bd50e2b2589f7d639
David Kezlinek <103315902+Dakevid@users.noreply.github.com>
2025-10-24 15:26:40 +0200

    Deduplicator - Add tests

---
+++ modules/deduplicator/tests/test.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+function exit_with_error {
+  pkill logger
+  pkill logreplay
+  pkill deduplicator
+  exit 1
+}
+
+function process_started {
+  pid=$1
+  if ! ps -p $pid > /dev/null
+    then
+      echo "Failed to start process"
+      exit_with_error
+  fi
+}
+
+data_path="$(dirname "$0")/testsData/"
+deduplicator=$1
+
+set -e
+trap 'echo "Command \"$BASH_COMMAND\" failed!"; exit_with_error' ERR
+for input_file in $data_path/inputs/*; do
+  index=$(echo "$input_file" | grep -o '[0-9]\+')
+  echo "Running test $index"
+
+  res_file="/tmp/res"
+  logger -i "u:deduplicator" -w $res_file &
+  logger_pid=$!
+  sleep 0.1
+
+  process_started $logger_pid
+
+  $deduplicator \
+    -i "u:din,u:deduplicator" &
+
+  detector_pid=$!
+  sleep 0.1
+  process_started $detector_pid
+
+  logreplay -i "u:din" -f "$data_path/inputs/input$index.csv" 2>/dev/null &
+  sleep 0.1
+  process_started $!
+
+  wait $logger_pid
+  wait $detector_pid
+
+  if [ -f "$res_file" ]; then
+    if ! cmp -s "$data_path/results/res$index.csv" "$res_file"; then
+      echo "Files results/res$index.csv and $res_file are not equal"
+      exit_with_error
+    fi
+  else
+    echo "File $res_file not found"
+    exit_with_error
+  fi
+done
+
+echo "All tests passed"
+exit 0
